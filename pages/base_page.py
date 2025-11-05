dcc178c8cab4075b598f9b90f957c9626a52b479
Miguel Ramos <return.infox@gmail.com>
2025-11-05 15:42:30 -0300

    base page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,2 +1,32 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import pytest
+
 class BasePage:
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+       
+    def open(self, url: str) -> None:
+        self.driver.get(url)
+    
+    def find_element(self, by, value):
+        return self.wait.until(
+            EC.presence_of_element_located((by, value))
+        )
+    
+    def find_clickable(self, by, value):
+        return self.wait.until(
+            EC.element_to_be_clickable((by, value))
+        )
+    
+    def is_element_visible(self, by, value, timeout=10):
+        try:
+            WebDriverWait(self.driver, timeout).until(
+                    EC.visibility_of_element_located((by, value))
+            )
+            return True
+        except TimeoutException:
+            return False
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,12 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
-    pass
+    # Locators
+    USERNAME_INPUT = (By.ID, "username") 
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+
+    def __init__(self, driver):
+        
+   
