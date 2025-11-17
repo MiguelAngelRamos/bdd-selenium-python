0e26b729b6d3438e478cf3171db61c651ab35a74
Miguel Ramos <return.infox@gmail.com>
2025-11-17 16:04:10 -0300

    codigo actualizado

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,25 +1,17 @@
 from pages.base_page import BasePage
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from pages.components.navbar_component import NavbarComponent
 
 class IndexPage(BasePage):
-
-    # Localizadores
     
-    CART_COUNT = (By.ID, "mini-cart-count")
-    # localizadores con condicion de login de usuario
-    CHECKOUT_LINK = (By.ID, "Checkout")
-    LOGOUT_BUTTON = (By.ID, "logout-btn")
-
-
     def __init__(self, driver, base_url):
         super().__init__(driver, base_url)
 
+        # 2. Composición "IndexPage tiene un NavbarComponent"
+        self.navbar = NavbarComponent(self.driver, self.base_url)
+
    
-    
 
-
-    def is_logout_button_visible(self):
-        return self.is_element_visible(*self.LOGOUT_BUTTON)
     
 
