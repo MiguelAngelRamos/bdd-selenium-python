65728439033fa8345cb0a7f9ee3e88680d8e4580
Miguel Ramos <return.infox@gmail.com>
2025-11-17 17:49:04 -0300

    codigo corregido

--- steps/navigation_steps.py
+++ steps/navigation_steps.py
@@ -18,9 +18,7 @@ def user_logged_in(selenium, base_url, username, password):
     # Esto confirma que el login fue exitoso y redirigio correctamente
     WebDriverWait(selenium, 10).until(EC.url_contains("index"))
 
-@given('el usuario está en la página principal')
-def usuario_en_pagina_principal(selenium, base_url):
-    selenium.get(f"{base_url}/index.html")
+
 
 @then('debería ver el menú de navegación')
 def ver_menu_navegacion(selenium, base_url):
@@ -44,22 +42,16 @@ def click_en_enlace(selenium, base_url, link_text):
     index_page = IndexPage(selenium, base_url)
 
     if link_text == "Productos":
-        assert index_page.navbar.click_products()
+        #!ERROR es por que aqui tenia assert eso paso por copiar y pegar el codigo no me fije que habia dejado los assert anteriores
+        index_page.navbar.click_products()
     elif link_text == "Carrito":
-        assert index_page.navbar.click_cart()
+        index_page.navbar.click_cart()
     else:
         raise ValueError(f"Step 'clic en enlace' no definido para: {link_text}")
 
 
 
-@then(parsers.parse('el usuario debería estar en la página "{page_name}"'))
-def usuario_en_pagina(selenium, page_name):
-    WebDriverWait(selenium, 15).until(
-        lambda d: page_name in d.current_url,
-        message=f"Esperaba estar en '{page_name} pero la URL ES: {selenium.current_url}"
-    )
-    assert page_name in selenium.current_url;
-    #TODO: Candidato para ir a los pasos Comunes
+
 
 @when("el usuario hace clic en cerrar sesión")
 def click_cerrar_sesion(selenium, base_url):
@@ -68,18 +60,3 @@ def click_cerrar_sesion(selenium, base_url):
     index_page.navbar.click_logout()
 
 
-@then("el localStorage debería estar vacio")
-def localstorage_vacio(selenium):
-    WebDriverWait(selenium, 10).until(
-        lambda selenium: (
-            selenium.execute_script("return localStorage.getItem('token');") in [None, "", "undefined", "null"] and
-            selenium.execute_script("return localStorage.getItem('user');") in [None, "", "undefined", "null"]
-        )
-    )
-
-    token = selenium.execute_script("return localStorage.getItem('token');")
-    user = selenium.execute_script("return localStorage.getItem('user');")
-    ## Assertions finales con mensajes descriptivos
-    assert token in [None, "", "undefined", "null"], f"Token deberia estar vacio pero es: {token}"
-    assert user in [None, "", "undefined", "null"], f"Token deberia estar vacio pero es: {user}"
-    #TODO: Candidato para ir a los pasos comunes
